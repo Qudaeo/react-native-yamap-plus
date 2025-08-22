bd68e9196676b00604fe8a1046b0675ff7996f76
Aleksey Pekhterev <qudaeo@yandex.ru>
2025-08-22 16:29:15 +0700

    fix: ios(new arc): fix marker state cleaning

--- ios/View/MarkerView.mm
+++ ios/View/MarkerView.mm
@@ -127,14 +127,15 @@ using namespace facebook::react;
 
 - (void)prepareForRecycle
 {
-  [super prepareForRecycle];
-  zIndex = 1;
-  scale = [NSNumber numberWithInt:1];
-  rotationType = [NSNumber numberWithInt:0];
-  visible = [NSNumber numberWithInt:1];
-  handled = NO;
-  source = nil;
-  lastSource = nil;
+    [super prepareForRecycle];
+    zIndex = 1;
+    scale = [NSNumber numberWithInt:1];
+    rotationType = [NSNumber numberWithInt:0];
+    visible = [NSNumber numberWithInt:1];
+    handled = NO;
+    source = nil;
+    lastSource = nil;
+    _reactSubviews = [[NSMutableArray alloc] init];
 }
 
 #else
