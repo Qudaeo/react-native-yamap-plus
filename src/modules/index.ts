a510f47ecb4b9998e8693314a69a6af8d001a6fd
Пехтерев Алексей Сергеевич <pehterev.a@galero.tech>
2026-01-28 09:32:41 +0700

    feat(ts): replace FigureParams union type (rn 0.84 issue)

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,6 +1,6 @@
-export {Search} from './Search';
-export {Suggest} from './Suggest';
-export {YamapInstance} from './YamapInstance';
-export {Transport} from './Transport';
-export {GeoFigureType, type FigureParams, SearchType, SearchSnippet} from '../spec/NativeSearchModule';
-export {type YamapSuggest, SuggestType} from '../spec/NativeSuggestsModule';
+export {Search} from './Search'
+export {Suggest} from './Suggest'
+export {YamapInstance} from './YamapInstance'
+export {Transport} from './Transport'
+export {type BoundingBox, type FigureParams, SearchType, SearchSnippet} from '../spec/NativeSearchModule'
+export {type YamapSuggest, SuggestType} from '../spec/NativeSuggestsModule'
