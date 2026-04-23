367992533006cac5ceb8603a035a77d37046b67d
-g <kirill@wowa.ca>
2026-04-23 17:52:25 -0400

    fix(cluster): сохраняем props.children рядом с renderMarker-маркерами

--- src/components/ClusteredYamap/ClusteredYamap.tsx
+++ src/components/ClusteredYamap/ClusteredYamap.tsx
@@ -1,22 +1,23 @@
-import React, {forwardRef, useMemo, useRef} from 'react';
-import {PixelRatio} from "react-native";
-import {getImageUri, processColorsToNative} from '../../utils';
+import React, { forwardRef, useMemo, useRef } from "react";
+import { PixelRatio } from "react-native";
+import { getImageUri, processColorsToNative } from "../../utils";
 import {
   onCameraPositionReceived,
   onScreenToWorldPointsReceived,
   onVisibleRegionReceived,
   onWorldToScreenPointsReceived,
-} from '../Yamap/events';
-import {type ClusteredYamapProps, type ClusteredYamapRef} from './types';
-import ClusteredYamapNativeComponent, {type ClusteredYamapNativeRef} from '../../spec/ClusteredYamapNativeComponent';
-import {useClusteredYamap} from '../../hooks/useYamap';
-import {Commands} from '../../spec/commands/yamap';
+} from "../Yamap/events";
+import { type ClusteredYamapProps, type ClusteredYamapRef } from "./types";
+import ClusteredYamapNativeComponent, {
+  type ClusteredYamapNativeRef,
+} from "../../spec/ClusteredYamapNativeComponent";
+import { useClusteredYamap } from "../../hooks/useYamap";
+import { Commands } from "../../spec/commands/yamap";
 
-export const ClusteredYamap = forwardRef<ClusteredYamapRef, ClusteredYamapProps>(({
-    clusterColor = 'red',
-    ...props
-  }, ref) => {
-
+export const ClusteredYamap = forwardRef<
+  ClusteredYamapRef,
+  ClusteredYamapProps
+>(({ clusterColor = "red", ...props }, ref) => {
   const nativeRef = useRef<ClusteredYamapNativeRef | null>(null);
 
   useClusteredYamap(nativeRef, ref, Commands);
@@ -24,48 +25,48 @@ export const ClusteredYamap = forwardRef<ClusteredYamapRef, ClusteredYamapProps>
   const nativeProps = useMemo(() => {
     const markers = props.clusteredMarkers ?? [];
     const renderMarker = props.renderMarker;
-    return processColorsToNative({
-      ...props,
-      onCameraPositionReceived,
-      onVisibleRegionReceived,
-      onWorldToScreenPointsReceived,
-      onScreenToWorldPointsReceived,
-      userLocationIcon: getImageUri(props.userLocationIcon),
-      clusterIcon: getImageUri(props.clusterIcon),
-      clusterColor,
-      clusteredMarkers: markers.map(mark => mark.point),
-      clusterSize: props.clusterSize ? {
-        width:  props.clusterSize.width
-          ? PixelRatio.getPixelSizeForLayoutSize(props.clusterSize.width)
-          : props.clusterSize.width,
-        height: props.clusterSize.height
-          ? PixelRatio.getPixelSizeForLayoutSize(props.clusterSize.height)
-          : props.clusterSize.height,
-      } : props.clusterSize,
-      clusterTextSize: props.clusterTextSize
-        ? PixelRatio.getPixelSizeForLayoutSize(props.clusterTextSize)
-        : props.clusterTextSize,
-      clusterTextYOffset: props.clusterTextYOffset
-        ? PixelRatio.getPixelSizeForLayoutSize(props.clusterTextYOffset)
-        : props.clusterTextYOffset,
-      clusterTextXOffset: props.clusterTextXOffset
-        ? PixelRatio.getPixelSizeForLayoutSize(props.clusterTextXOffset)
-        : props.clusterTextXOffset,
-      children: renderMarker ? markers.map(renderMarker) : undefined,
-    }, [
-      'clusterColor',
-      'userLocationAccuracyFillColor',
-      'userLocationAccuracyStrokeColor',
-      'clusterTextColor'
-    ]);
-  },
-    [clusterColor, props]
-  );
+    return processColorsToNative(
+      {
+        ...props,
+        onCameraPositionReceived,
+        onVisibleRegionReceived,
+        onWorldToScreenPointsReceived,
+        onScreenToWorldPointsReceived,
+        userLocationIcon: getImageUri(props.userLocationIcon),
+        clusterIcon: getImageUri(props.clusterIcon),
+        clusterColor,
+        clusteredMarkers: markers.map((mark) => mark.point),
+        clusterSize: props.clusterSize
+          ? {
+              width: props.clusterSize.width
+                ? PixelRatio.getPixelSizeForLayoutSize(props.clusterSize.width)
+                : props.clusterSize.width,
+              height: props.clusterSize.height
+                ? PixelRatio.getPixelSizeForLayoutSize(props.clusterSize.height)
+                : props.clusterSize.height,
+            }
+          : props.clusterSize,
+        clusterTextSize: props.clusterTextSize
+          ? PixelRatio.getPixelSizeForLayoutSize(props.clusterTextSize)
+          : props.clusterTextSize,
+        clusterTextYOffset: props.clusterTextYOffset
+          ? PixelRatio.getPixelSizeForLayoutSize(props.clusterTextYOffset)
+          : props.clusterTextYOffset,
+        clusterTextXOffset: props.clusterTextXOffset
+          ? PixelRatio.getPixelSizeForLayoutSize(props.clusterTextXOffset)
+          : props.clusterTextXOffset,
+        children: renderMarker
+          ? [props.children, ...markers.map(renderMarker)]
+          : props.children,
+      },
+      [
+        "clusterColor",
+        "userLocationAccuracyFillColor",
+        "userLocationAccuracyStrokeColor",
+        "clusterTextColor",
+      ],
+    );
+  }, [clusterColor, props]);
 
-  return (
-    <ClusteredYamapNativeComponent
-      {...nativeProps}
-      ref={nativeRef}
-    />
-  );
+  return <ClusteredYamapNativeComponent {...nativeProps} ref={nativeRef} />;
 });
